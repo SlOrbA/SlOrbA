5cbc9f8535085766cd7d92be0806b0c48292b932
Lari Korpi <lari.korpi@tieto.com>
2021-10-26 15:19:18 +0300

    added Android project

---
+++ app/src/main/java/net/hlan/slorba/app/MainActivity.java
@@ -0,0 +1,13 @@
+package net.hlan.slorba.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
